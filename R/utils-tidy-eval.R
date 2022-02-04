ae9d0fdfe6d609dd30c6fd777cae953b1a1b4a05
upulcooray <upulcooray@gmail.com>
2022-02-04 18:27:56 +0900

    Removed tidyeval exports

--- R/utils-tidy-eval.R
+++ R/utils-tidy-eval.R
@@ -103,5 +103,4 @@
 #' @keywords internal
 #' @importFrom rlang enquo enquos .data := as_name as_label
 #' @aliases enquo enquos .data := as_name as_label
-#' @export enquo enquos .data := as_name as_label
 NULL
